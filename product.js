e6238eb4f15d6cac86b9ab3f551a01c18a64ce3b
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2022-10-24 18:07:00 +0500

    product 24-oct

--- product.js
+++ product.js
@@ -1,18 +1,47 @@
 import React from "react";
-import { useParams, useNavigate , useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import { Backdrop } from "@mui/material";
+// import { useState } from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Card2Component from "../../components/card/card2";
 
 export default function Product(props) {
-    const { productID } = useParams();
-    const location = useLocation()
-    
-    //location is used to access data sent through useNavigate using state as second parameter 
-    //sent from cards2 line no. 83 {i.e navigate(`/product/${productID}`, { state: { cardData } }); }
-console.log(location.state);
-  
+  const { productID } = useParams();
+  const location = useLocation();
+
+  //   let [open, setOpen] = useState(true);
+
+  //location is used to access data sent through useNavigate using state as second parameter
+  //sent from cards2 line no. 83 {i.e navigate(`/product/${productID}`, { state: { cardData } }); }
+
+  console.log(location.state);
+
   return (
     <>
-      <h1>my {productID}</h1>
+      {/* <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      ></Backdrop> */}
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: 1,
+            width: 128,
+            height: 128
+          }
+        }}
+      >
+        <Card2Component
+          id={location.state.cardData.id}
+          image={location.state.cardData.image}
+          title={location.state.cardData.title}
+          price={location.state.cardData.price}
+          description={location.state.cardData.description}
+        ></Card2Component>
+      </Box>
     </>
   );
 }
