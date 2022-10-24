704ca80699c6d1642aafa31c29fb32782d757d84
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2022-10-24 17:41:49 +0500

    index router 24-oct

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Home from '../../screens/home/home'
 import TodoApp from '../../screens/todoApp/todoApp'
+import CardData from '../../assets/data/cardData'
+import Product from '../../screens/product/product'
+import PageNotFound from '../../screens/pageNotFound/pageNotFound'
 
-
-export  {Home,TodoApp}
+export  {Home,TodoApp,CardData,Product,PageNotFound}
