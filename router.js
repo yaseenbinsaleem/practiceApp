fe3d8e3d689232ca206f468ddc59b7a4758e92c7
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2022-10-24 17:41:26 +0500

    router 24-oct

--- router.js
+++ router.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import { Home,TodoApp} from ".";
+import { Home, TodoApp, CardData, Product, PageNotFound } from ".";
 
 export default function Router() {
-    return(
-        <>
-        <Routes>
-          <Route index path="/" element={<Home />}></Route>
-          <Route path="/todoApp" element={<TodoApp />}></Route>
-        </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route index path="/" element={<Home />}></Route>
+        <Route path="/todoApp" element={<TodoApp />}></Route>
+        <Route path="/cardData" element={<CardData />}></Route>
+        <Route path="/product/:productID" element={<Product />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </>
+  );
 }
- 
