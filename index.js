837e1343b20dc1337e8b4371823e4329aec9e098
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2022-10-25 18:12:38 +0500

    index.js 25 oct

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import TodoApp from '../../screens/todoApp/todoApp'
 import CardData from '../../assets/data/cardData'
 import Product from '../../screens/product/product'
 import PageNotFound from '../../screens/pageNotFound/pageNotFound'
+import Report from '../../screens/report/report'
 
-export  {Home,TodoApp,CardData,Product,PageNotFound}
+export  {Home,TodoApp,CardData,Product,PageNotFound,Report}
